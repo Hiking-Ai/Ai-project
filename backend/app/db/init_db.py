d7a5aa6d2a24cd04b3ed7c12180bb0246a03b1d2
kimhanyoung <khy930324@gmail.com>
2025-06-17 16:33:40 +0900

    수정

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -2,7 +2,7 @@
 from app.db.base import Base
 from app.db.session import engine
 
-from app.models import user, user_profile, comment, category, post_category, post, signup_token
+from app.models import user, user_profile, comment, category, post_category, post, signup_token, favorite
 
 def init_db():
     print("Creating tables in MySQL...")
--- backend/app/models/favorite.py
+++ backend/app/models/favorite.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey, UniqueConstraint
+from sqlalchemy import Column, Integer, ForeignKey, UniqueConstraint,DateTime
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.db.base import Base
@@ -8,7 +8,7 @@ class Favorite(Base):
 
     user_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), primary_key=True)
     post_id = Column(Integer, ForeignKey("post.post_id", ondelete="CASCADE" ), primary_key=True)
-    created_at = Column(datetime, default=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.utcnow)
 
     user = relationship("User", back_populates="favorites")
     post = relationship("Post", back_populates="favorites")
